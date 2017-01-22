c1e047514e35eb680520923d917a07735c73feb3
Svetoslav Tsenov <tsenov.svetoslav@gmail.com>
2017-01-22 23:21:29 +0200

    Update

--- src/test/java/hybridapp/pages/HomePage.java
+++ src/test/java/hybridapp/pages/HomePage.java
@@ -1,4 +1,30 @@
 package hybridapp.pages;
 
-public class HomePage {
+import functional.tests.core.basepage.BasePage;
+import functional.tests.core.element.UIRectangle;
+
+public class HomePage extends BasePage {
+
+    private String tapBtnImageMap = "tap";
+
+    public HomePage()
+    {
+        super();
+    }
+
+    /**
+     * The "TAP" button.
+     *
+     * @return The "TAP" button.
+     */
+    private UIRectangle button() {
+        return this.context.sikuliImageProcessing.findImageOnScreen(tapBtnImageMap, 0.7);
+    }
+
+    /**
+     * Tap the "TAP" button.
+     */
+    public void tapButton() {
+        this.button().tap();
+    }
 }
--- src/test/java/hybridapp/tests/SmokeTests.java
+++ src/test/java/hybridapp/tests/SmokeTests.java
@@ -1,4 +1,17 @@
 package hybridapp.tests;
 
-public class SmokeTests {
+import functional.tests.core.basetest.UIBaseTest;
+import nativeapp.pages.HomePage;
+import org.testng.annotations.Test;
+
+public class SmokeTests extends UIBaseTest {
+
+    @Test(description = "Verify home page looks correct",
+            groups = {"android", "ios"})
+    public void test_01_smoke() throws Exception {
+        HomePage homePage = new HomePage();
+        homePage.tapButton();
+
+        this.assertScreen(2);
+    }
 }
